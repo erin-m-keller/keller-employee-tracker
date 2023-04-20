4867186d6f582bcc7124366bed7e27977583d43c
Erin Keller <erinkeller@Erins-MacBook-Pro-2.local>
2023-04-20 16:47:24 -0400

    Started building Employee class, added method to show all Employees

--- index.js
+++ index.js
@@ -1,77 +1,137 @@
 require('dotenv').config()
-const mysql = require('mysql2'),
+const connection = require('./lib/db'),
       inquirer = require('inquirer'),
+      Employee = require('./lib/employee'),
       asciiArt = `
-    .-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-.
-    |                                                                            |
-    |   ███████╗███╗   ███╗██████╗ ██╗      ██████╗ ██╗   ██╗███████╗███████╗    |
-    |   ██╔════╝████╗ ████║██╔══██╗██║     ██╔═══██╗╚██╗ ██╔╝██╔════╝██╔════╝    |
-    |   █████╗  ██╔████╔██║██████╔╝██║     ██║   ██║ ╚████╔╝ █████╗  █████╗      |
-    |   ██╔══╝  ██║╚██╔╝██║██╔═══╝ ██║     ██║   ██║  ╚██╔╝  ██╔══╝  ██╔══╝      |
-    |   ███████╗██║ ╚═╝ ██║██║     ███████╗╚██████╔╝   ██║   ███████╗███████╗    |
-    |   ╚══════╝╚═╝     ╚═╝╚═╝     ╚══════╝ ╚═════╝    ╚═╝   ╚══════╝╚══════╝    |
-    |                                                                            |
-    |   ██████╗  █████╗ ████████╗ █████╗ ██████╗  █████╗ ███████╗███████╗        |
-    !   ██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔════╝        !
-    :   ██║  ██║███████║   ██║   ███████║██████╔╝███████║███████╗█████╗          :
-    :   ██║  ██║██╔══██║   ██║   ██╔══██║██╔══██╗██╔══██║╚════██║██╔══╝          :
-    :   ██████╔╝██║  ██║   ██║   ██║  ██║██████╔╝██║  ██║███████║███████╗        :
-    :   ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝        :
-    .                                                                            .
-    .   ███╗   ███╗ █████╗ ███╗   ██╗ █████╗  ██████╗ ███████╗██████╗            .
-    .   ████╗ ████║██╔══██╗████╗  ██║██╔══██╗██╔════╝ ██╔════╝██╔══██╗           .
-    .   ██╔████╔██║███████║██╔██╗ ██║███████║██║  ███╗█████╗  ██████╔╝           .
-    :   ██║╚██╔╝██║██╔══██║██║╚██╗██║██╔══██║██║   ██║██╔══╝  ██╔══██╗           :
-    !   ██║ ╚═╝ ██║██║  ██║██║ ╚████║██║  ██║╚██████╔╝███████╗██║  ██║           !
-    |                                                                            |
-    .-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-.
+        .-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-.
+        |                                                                            |
+        |   ███████╗███╗   ███╗██████╗ ██╗      ██████╗ ██╗   ██╗███████╗███████╗    |
+        |   ██╔════╝████╗ ████║██╔══██╗██║     ██╔═══██╗╚██╗ ██╔╝██╔════╝██╔════╝    |
+        |   █████╗  ██╔████╔██║██████╔╝██║     ██║   ██║ ╚████╔╝ █████╗  █████╗      |
+        |   ██╔══╝  ██║╚██╔╝██║██╔═══╝ ██║     ██║   ██║  ╚██╔╝  ██╔══╝  ██╔══╝      |
+        |   ███████╗██║ ╚═╝ ██║██║     ███████╗╚██████╔╝   ██║   ███████╗███████╗    |
+        |   ╚══════╝╚═╝     ╚═╝╚═╝     ╚══════╝ ╚═════╝    ╚═╝   ╚══════╝╚══════╝    |
+        |                                                                            |
+        |   ██████╗  █████╗ ████████╗ █████╗ ██████╗  █████╗ ███████╗███████╗        |
+        !   ██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔════╝        !
+        :   ██║  ██║███████║   ██║   ███████║██████╔╝███████║███████╗█████╗          :
+        :   ██║  ██║██╔══██║   ██║   ██╔══██║██╔══██╗██╔══██║╚════██║██╔══╝          :
+        :   ██████╔╝██║  ██║   ██║   ██║  ██║██████╔╝██║  ██║███████║███████╗        :
+        :   ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝        :
+        .                                                                            .
+        .   ███╗   ███╗ █████╗ ███╗   ██╗ █████╗  ██████╗ ███████╗██████╗            .
+        .   ████╗ ████║██╔══██╗████╗  ██║██╔══██╗██╔════╝ ██╔════╝██╔══██╗           .
+        .   ██╔████╔██║███████║██╔██╗ ██║███████║██║  ███╗█████╗  ██████╔╝           .
+        :   ██║╚██╔╝██║██╔══██║██║╚██╗██║██╔══██║██║   ██║██╔══╝  ██╔══██╗           :
+        !   ██║ ╚═╝ ██║██║  ██║██║ ╚████║██║  ██║╚██████╔╝███████╗██║  ██║           !
+        |                                                                            |
+        .-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-.
       `;
-
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: process.env.DB_PASSWORD,
-    database: 'employees_db'
-});
 
 connection.connect((err) => {
     if (err) {
         console.error(`Error connecting to ${connection.config.database} database:` + err.stack);
         return;
     }
-    console.log(`Connected to ${connection.config.database} database.`);
-    console.log(asciiArt);
+    console.log(`Successfully connected to ${connection.config.database} database.`);
+    terminalPrompt();
 });
 
 // Create the prompts
-const questions = () => {
+const applicationPrompts = () => {
     return inquirer.prompt([
         {
-            type: 'input',
-            name: 'title',
-            message: 'What would you like the title to be for your application?',
-            validate: validateInput 
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                { name: "View All Employees", value: "all_emp"},
+                { name: "View Employees by Manager (bonus)", value: "emp_by_mgr"},
+                { name: "View Employees by Department (bonus)", value: "emp_by_dept"},
+                { name: "Add Employee", value: "add_emp"},
+                { name: "Delete Employee (bonus)", value: "delete_emp"},
+                { name: "Update Employee Role", value: "update_emp_role"},
+                { name: "Update Employee Manager (bonus)", value: "update_emp_mgr"},
+                { name: "View All Roles", value: "all_roles"},
+                { name: "Add Role", value: "add_role"},
+                { name: "Delete Role (bonus)", value: "delete_role"},
+                { name: "View All Departments", value: "all_depts"},
+                { name: "Add Department", value: "add_dept"},
+                { name: "Delete Department (bonus)", value: "delete_dpt"},
+                { name: "Exit", value: "exit"},
+            ]
+            
         }
     ])
 };
 
-const validateInput = async (input) => {
-    if (input) {
-        return true;
-    } 
-    return 'You must enter the information.';
-};
-
-function init() {
-    //questions().then(answers => {
-        // do something
-    //}).then(data => {
-        // do something
-    //}).catch(err => {
+function terminalPrompt() {
+    console.log('\x1b[31m%s\x1b[0m',asciiArt);
+    applicationPrompts().then(response => {
+        let employeeClass = new Employee();
+        switch (response.action) {
+            case 'all_emp': {
+                employeeClass.viewAllEmployees(terminalPrompt);
+                break;
+            }
+            case 'emp_by_mgr': {
+                console.info('View Employees by Manager - TODO (bonus)');
+                break;
+            }
+            case 'emp_by_dept': {
+                console.info('View Employees by Department - TODO (bonus)');
+                break;
+            }
+            case 'add_emp': {
+                console.info('Add Employee - TODO');
+                break;
+            }
+            case 'delete_emp': {
+                console.info('Delete Employee - TODO (bonus)');
+                break;
+            }
+            case 'update_emp_role': {
+                console.info('Update Employee Role - TODO');
+                break;
+            }
+            case 'update_emp_mgr': {
+                console.info('Update Employee Manager - TODO (bonus)');
+                break;
+            }
+            case 'all_roles': {
+                console.info('View All Roles - TODO');
+                break;
+            }
+            case 'add_role': {
+                console.info('Add Role - TODO');
+                break;
+            }
+            case 'delete_role': {
+                console.info('Delete Role - TODO (bonus)');
+                break;
+            }
+            case 'all_depts': {
+                console.info('View All Departments - TODO');
+                break;
+            }
+            case 'add_dept': {
+                console.info('Add Department - TODO');
+                break;
+            }
+            case 'delete_dpt': {
+                console.info('Delete Department - TODO (bonus)');
+                break;
+            }
+            case 'exit': {
+                console.info('Exit the application - TODO');
+                break;
+            }
+            default:
+                // default
+            }
+    }).catch(err => {
         // console log the error
-        //console.log("Error: " + err)
-    //});
+        console.log(`Error: ${err}`)
+    });
 }
-
-// initialize the application
-init();
+  
