4534baf1006f1880862a44711fbadbbf3e59af40
Erin Keller <erinkeller@Erins-MacBook-Pro-2.local>
2023-04-21 11:23:01 -0400

    Updated color of art

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ const applicationPrompts = () => {
 };
 
 function terminalPrompt() {
-    console.log('\x1b[31m%s\x1b[0m',asciiArt);
+    console.log('\x1b[35m%s\x1b[0m',asciiArt);
     applicationPrompts().then(response => {
         let employeeClass = new Employee(),
             departmentClass = new Department();
